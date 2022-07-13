5a2496bc5bdc03b10143e6d66f781672832649b7
Jamshid <jamshidtolibjonov3323@gmail.com>
2022-07-13 17:25:26 +0500

    added app

--- config/settings.py
+++ config/settings.py
@@ -18,6 +18,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'pages',
+    'orders',
+    'shop',
+    'users',
+    'blogs'
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
+    path('shop/', include('shop.urls')),
+    path('blog/', include('blogs.urls')),
+    path('order/', include('orders.urls')),
+    path('', include('users.urls')),
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
