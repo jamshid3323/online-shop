3158f011cb31fd8b10e42f36f222415c26ca1142
Jamshid <jamshidtolibjonov3323@gmail.com>
2022-07-25 14:51:29 +0500

    price low to high and high to low added

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import ProductModel, CategoryModel, ProductTagModel
+from .models import ProductModel, CategoryModel, ProductTagModel, ProductColorModel, ProductBrandModel, ProductSizeModel
 
 
 class ShopView(ListView):
@@ -20,13 +20,33 @@ class ShopView(ListView):
         tag = self.request.GET.get('tag')
         if tag:
             qs = qs.filter(id=tag)
+
+        brand = self.request.GET.get('brand')
+        if brand:
+            qs = qs.filter(brand=brand)
+
+        size = self.request.GET.get('size')
+        if size:
+            qs = qs.filter(size=size)
+
+        color = self.request.GET.get('color')
+        if color:
+            qs = qs.filter(color=color)
+
+        sort = self.request.GET.get('sort')
+        if sort == 'price':
+            qs = qs.order_by('price')
+        elif sort == '-price':
+            qs = qs.order_by('-price')
         return qs
 
     def get_context_data(self, *, object_list=None, **kwargs):
         data = super().get_context_data()
         data['categories'] = CategoryModel.objects.all()
         data['tags'] = ProductTagModel.objects.all()
-
+        data['brands'] = ProductBrandModel.objects.all()
+        data['sizes'] = ProductSizeModel.objects.all()
+        data['colors'] = ProductColorModel.objects.all()
 
         return data
 
