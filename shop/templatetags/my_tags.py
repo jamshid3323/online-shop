897a7134e9c4a9c1312b0543707d0e50af541005
Jamshid <jamshidtolibjonov3323@gmail.com>
2022-07-30 15:50:55 +0500

    remove cart done

--- shop/templatetags/my_tags.py
+++ shop/templatetags/my_tags.py
@@ -16,3 +16,8 @@ def get_current_price(request, x):
 @register.filter()
 def is_wishlist(product, request):
     return WishlistModel.objects.filter(user=request.user, product=product).exists()
+
+
+@register.filter()
+def is_cart(product, request):
+    return product.id in request.session.get('cart', [])
